54be52f48d7b1dc695a1eb8cbad0fa3099af5eb1
Cyan Thayn <git@thayn.me>
2020-10-05 23:59:53 -0500

    Home page can now be populated from C#

--- InventorySystem/Home.xaml.cs
+++ InventorySystem/Home.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -11,6 +12,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
@@ -25,6 +27,31 @@ namespace InventorySystem
         public Home()
         {
             this.InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                var url = "ms-appx:///Assets/Wide310x150Logo.scale-200.png";
+                //var url = "C:/Users/cyan/OneDrive/Pictures/Annotation2.png";
+                Image img = new Image();
+                img.Source = new BitmapImage(new Uri(url));
+
+                Thickness margin = new Thickness(5, 0, 5, 10);
+
+                GridViewItem gvi = new GridViewItem
+                {
+                    MaxWidth = 500,
+                    Margin = margin,
+                    Background = new SolidColorBrush(Windows.UI.Colors.Gray),
+                    //Content = "GridViewItem " + i
+                    Content = img
+                };
+
+                //SampleList.Items.Add(img);
+                SampleList.Items.Add(gvi);
+            }
         }
     }
 }
