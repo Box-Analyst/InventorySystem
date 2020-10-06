c195a0675f6f9a90ace37090bd56d25c2a6c3bbb
Cyan Thayn <git@thayn.me>
2020-10-06 00:43:34 -0500

    Added Page_SizeChanged stuff. Also removed template stuff.

--- InventorySystem/Home.xaml.cs
+++ InventorySystem/Home.xaml.cs
@@ -15,13 +15,8 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
-
 namespace InventorySystem
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class Home : Page
     {
         public Home()
@@ -41,7 +36,8 @@ namespace InventorySystem
 
                 GridViewItem gvi = new GridViewItem
                 {
-                    MaxWidth = 500,
+                    //MaxWidth = 500,
+                    MaxWidth = (Frame.ActualWidth / 2) - 10,
                     Margin = margin,
                     Background = new SolidColorBrush(Windows.UI.Colors.Gray),
                     //Content = "GridViewItem " + i
@@ -51,6 +47,12 @@ namespace InventorySystem
                 //SampleList.Items.Add(img);
                 SampleList.Items.Add(gvi);
             }
+        }
+
+        private void Page_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            // Change GridViewItem's MaxWidth on window resize
+            //GridViewItem.Items.MaxWidth = (Frame.ActualWidth / 2) - 10;
         }
     }
 }
