d67a507410db3527a89c769fcced871934501216
dgray22 <63367983+dgray22@users.noreply.github.com>
2020-10-12 16:18:19 -0500

    Added LoginWindow page

--- InventorySystem/App.xaml.cs
+++ InventorySystem/App.xaml.cs
@@ -117,7 +117,7 @@ namespace InventorySystem
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(LoginWindow), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
