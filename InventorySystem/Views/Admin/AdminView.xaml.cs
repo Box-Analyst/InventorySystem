44e0abb0674ec26646edf441311522106c3f90c5
Cyan Thayn <git@thayn.me>
2020-10-16 19:45:52 -0500

    Fixed ManageDB.Add_Text implementation.

--- InventorySystem/Views/Admin/AdminView.xaml.cs
+++ InventorySystem/Views/Admin/AdminView.xaml.cs
@@ -16,35 +16,9 @@ namespace InventorySystem.Views.Admin
             Output.ItemsSource = SQL.ManageDB.Grab_Entries_col();
         }
 
-        //private void Add_Text(object sender, RoutedEventArgs e)
-        //{
-        //    SQL.ManageDB.Add_Text(sender, e);
-        //    Output.ItemsSource = SQL.ManageDB.Grab_Entries_col();
-        //}
-
-        // Method to insert text into the SQLite database
         private void Add_Text(object sender, RoutedEventArgs e)
         {
-            using (SqliteConnection db = new SqliteConnection("Filename=sqliteSample.db"))
-            {
-                db.Open();
-                SqliteCommand insertCommand = new SqliteCommand();
-                insertCommand.Connection = db;
-
-                //Use parameterized query to prevent SQL injection attacks
-                insertCommand.CommandText = "INSERT INTO MyTable VALUES (NULL, @Entry);";
-                insertCommand.Parameters.AddWithValue("@Entry", Input_Box.Text);
-                try
-                {
-                    insertCommand.ExecuteReader();
-                }
-                catch (SqliteException error)
-                {
-                    //Handle error
-                    return;
-                }
-                db.Close();
-            }
+            SQL.ManageDB.Add_Text(sender, e, Input_Box.Text);
             Output.ItemsSource = SQL.ManageDB.Grab_Entries_col();
         }
     }
---
+++ InventorySystem/Views/Settings/Components/Settings.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventorySystem.Views.Settings.Components
+{
+    class Settings
+    {
+        // Method for modifying app settings
+        public static string ModifySetting(string SettingName, string SettingValue)
+        {
+            Windows.Storage.ApplicationData.Current.LocalSettings.Values[SettingName] = SettingValue;
+            return (string)Windows.Storage.ApplicationData.Current.LocalSettings.Values[SettingName];
+        }
+
+        // Method for fetching app settings
+        public static object FetchSetting(object SettingName)
+        {
+            if (SettingName == null)
+            {
+                return null;
+            }
+            else
+            {
+                return (string)Windows.Storage.ApplicationData.Current.LocalSettings.Values[(string)SettingName];
+            }
+        }
+
+        // Method for deleting app settings
+        public static void RemoveSetting(string SettingName)
+        {
+            Windows.Storage.ApplicationData.Current.LocalSettings.Values.Remove(SettingName);
+        }
+    }
+}
