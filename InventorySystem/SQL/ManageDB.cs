c3b53cb27fdfe6164246adcd8eec43835eabb0dd
AnalogCyan <git@thayn.me>
2020-10-26 17:17:48 -0500

    Merge JD-GUI into master (#18)
    * Actually searches the database now!
    
    * Dynamic NavView DisplayMode based on window size.
    
    * Centered content & added add AppBarButton.
    
    * .gitignore is now working (#8)
    
    Added [VisualStudio.gitignore](https://github.com/github/gitignore/blob/master/VisualStudio.gitignore) so VS-generated temporary files, build results, etc. don't clutter the repo.
    
    * Add button actually works now...
    This took way too long to figure out 😒
    
    * Pages can actually scroll content now.
    The parent element for all pages was inside a StackPanel... 🤦‍♀️
    
    * Home page can now be populated from C#
    
    * Cleanup & minor fixes
    
    * Added Page_SizeChanged stuff.
    Also removed template stuff.
    
    * Added setting page & theme changer.
    
    * Lowered minimum required version.
    
    * Notify icon.
    
    * Notifications & begining of organization.
    
    * Add page popup works now...
    oh and UI organization I guess
    
    * Organized all the things.
    
    * Removed old files.
    
    * Fixed broken changes from merge.
    
    * Fixed ManageDB.Add_Text implementation.
    
    * Removed old comments.
    
    * Code cleanup.
    
    * Fixed potential System.NullReferenceException cases.
    
    * Start of HomeView design.
    
    * More UI testing.
    
    * Case insensitive search.
    
    * Updated ManageDB, AddSample
    
    Functions in ManageDB to Add Samples into the database. Updated Table creation code to create appropriate database. Fixed Add Sample page.
    
    * Update ManageDB.cs
    
    fixed lotnumber name
    
    * Fixed Problems
    
    * stuff
    
    * Stuff idk
    
    Co-authored-by: jchrestman2 <64033718+jchrestman2@users.noreply.github.com>

--- InventorySystem/SQL/ManageDB.cs
+++ InventorySystem/SQL/ManageDB.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using Microsoft.Data.Sqlite;
 using Windows.UI.Xaml;
+using System.IO;
+using Windows.Storage;
+using System.Runtime.InteropServices;
+using Windows.UI.WindowManagement;
 
 namespace InventorySystem.SQL
 {
@@ -11,10 +15,11 @@ namespace InventorySystem.SQL
         public static void InitializeDB()
         {
             //SqliteEngine.UseWinSqlite3(); //Configuring library to use SDK version of SQLite
-            using (SqliteConnection db = new SqliteConnection("Filename=sqliteSample.db")) //Name of .db file doesn't matter, but should be consistent across all SqliteConnection objects
+            using (SqliteConnection db = new SqliteConnection("Filename=SamplesDB.db")) //Name of .db file doesn't matter, but should be consistent across all SqliteConnection objects
             {
                 db.Open(); //Open connection to database
-                const string tableCommand = "CREATE TABLE IF NOT EXISTS MyTable (Primary_Key INTEGER PRIMARY KEY AUTOINCREMENT, Text_Entry NVARCHAR(2048) NULL)";
+
+                string tableCommand = "CREATE TABLE IF NOT EXISTS Login (Emp_id NUMERIC PRIMARY KEY NOT NULL UNIQUE, Pin VARCHAR (6) NOT NULL, IsActive BOOLEAN NOT NULL)";
                 SqliteCommand createTable = new SqliteCommand(tableCommand, db);
                 try
                 {
@@ -22,19 +27,43 @@ namespace InventorySystem.SQL
                 }
                 catch (SqliteException e)
                 {
-                    //Do nothing
+                    Console.WriteLine("Table error: " + e);
                 }
+                tableCommand = "CREATE TABLE IF NOT EXISTS Sample (LotNum VARCHAR PRIMARY KEY NOT NULL UNIQUE, NameandDosage VARCHAR(50) NOT NULL, Count INTEGER NOT NULL, ExpirationDate VARCHAR NOT NULL, isExpired BOOLEAN NOT NULL)";
+                createTable = new SqliteCommand(tableCommand, db);
+                try
+                {
+                    createTable.ExecuteReader(); //Execute command, throws SqliteException error if command doesn't execute properly
+                }
+                catch (SqliteException e)
+                {
+                    Console.WriteLine("Table error: " + e);
+                }
+                tableCommand = "CREATE TABLE IF NOT EXISTS Log (LogEntryId INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, Emp_id INTEGER CONSTRAINT Emp_idConstraint REFERENCES Login (Emp_id) ON DELETE NO ACTION ON UPDATE CASCADE NOT NULL, LotNum VARCHAR REFERENCES Sample (LotNum) ON DELETE NO ACTION ON UPDATE CASCADE NOT NULL, WhenModifed DATETIME NOT NULL, Patient_id VARCHAR (12), Rep_id VARCHAR (12), LogType CHAR (1) NOT NULL)";
+                createTable = new SqliteCommand(tableCommand, db);
+                try
+                {
+                    createTable.ExecuteReader(); //Execute command, throws SqliteException error if command doesn't execute properly
+                }
+                catch (SqliteException e)
+                {
+                    Console.WriteLine("Table error: " + e);
+                }
+                db.Close();
             }
         }
 
+
         // Method to grab entries from the SQLite database
-        public static List<string> Grab_Entries(string search)
+        public static List<string> Grab_Entries(string table, string column, object search)
         {
             List<string> entries = new List<string>();
-            using (SqliteConnection db = new SqliteConnection("Filename=sqliteSample.db"))
+            using (SqliteConnection db = new SqliteConnection("Filename=SamplesDB.db"))
             {
                 db.Open();
-                SqliteCommand selectCommand = new SqliteCommand("SELECT Text_Entry from MyTable", db);
+                SqliteCommand selectCommand = new SqliteCommand("SELECT @Column FROM @Table", db);
+                selectCommand.Parameters.AddWithValue("@Table", table);
+                selectCommand.Parameters.AddWithValue("@Column", column);
                 SqliteDataReader query;
                 try
                 {
@@ -42,26 +71,64 @@ namespace InventorySystem.SQL
                 }
                 catch (SqliteException error)
                 {
-                    //Handle error
+                    Console.WriteLine("Error: " + error);
+                    db.Close();
                     return entries;
                 }
                 while (query.Read())
                 {
-                    var tmp = query.GetString(0);
-                    if (tmp.Contains(search))
+                    // if search is specified, only return values containing that query
+                    // otherwise return entire column
+                    if (search != null && query.GetString(0).ToLower().Contains(search.ToString().ToLower()))
                     {
-                        entries.Add(tmp);
+                        entries.Add(query.GetString(0));
+                    }
+                    else
+                    {
+                        entries.Add(query.GetString(0));
                     }
                 }
                 db.Close();
             }
             return entries;
         }
+        // Method to insert Samples into the Sample table
+        // Needs to check if Sample exists
 
-        // Method to insert text into the SQLite database
-        public static void Add_Text(object sender, RoutedEventArgs e, string inputVal)
+        public static bool Check_Sample(object sender, RoutedEventArgs e, string LotNumber)
         {
-            using (SqliteConnection db = new SqliteConnection("Filename=sqliteSample.db"))
+            bool check = false;
+            using (SqliteConnection db = new SqliteConnection("Filename=SamplesDB.db"))
+            {
+                db.Open();
+                SqliteCommand selectCommand = new SqliteCommand("SELECT LotNum FROM Sample", db);
+                SqliteDataReader query;
+                try
+                {
+                    query = selectCommand.ExecuteReader();
+                }
+                catch (SqliteException error)
+                {
+                    Console.WriteLine("Exception:" + error);
+                    db.Close();
+                    return false;
+                }
+                while (query.Read())
+                {
+                    var tmp = query.GetString(0);
+                    if (tmp.Contains(LotNumber))
+                    {
+                        check = true;
+                    }
+                }
+                db.Close();
+            }
+            return check;
+        }
+        public static bool Update_Sample(object sender, RoutedEventArgs e, string LotNumber, int count)
+        {
+            bool check = true;
+            using (SqliteConnection db = new SqliteConnection("Filename=SamplesDB.db"))
             {
                 db.Open();
                 SqliteCommand insertCommand = new SqliteCommand
@@ -69,7 +136,110 @@ namespace InventorySystem.SQL
                     Connection = db,
 
                     //Use parameterized query to prevent SQL injection attacks
-                    CommandText = "INSERT INTO MyTable VALUES (NULL, @Entry);"
+                    CommandText = "UPDATE Sample SET Count = @Entry2 + Count WHERE LotNum = @Entry1;"
+                };
+                insertCommand.Parameters.AddWithValue("@Entry1", LotNumber);
+                insertCommand.Parameters.AddWithValue("@Entry2", count);
+                try
+                {
+                    insertCommand.ExecuteReader();
+                }
+                catch (SqliteException error)
+                {
+                    Console.WriteLine(error);
+                    check = false;
+                }
+                db.Close();
+            }
+            return check;
+        }
+        public static bool Add_Sample(object sender, RoutedEventArgs e, string LotNumber, string NameandDosage, int count, string expirationdate, bool isExpired)
+        {
+            bool check = true;
+            if (Check_Sample(sender, e, LotNumber))
+            {
+                Update_Sample(sender, e, LotNumber, count);
+                return true;
+            }
+            using (SqliteConnection db = new SqliteConnection("Filename=SamplesDB.db"))
+            {
+                db.Open();
+                SqliteCommand insertCommand = new SqliteCommand
+                {
+                    Connection = db,
+
+                    //Use parameterized query to prevent SQL injection attacks
+                    CommandText = "INSERT INTO Sample VALUES (@Entry1, @Entry2, @Entry3, @Entry4, @Entry5);"
+                };
+                insertCommand.Parameters.AddWithValue("@Entry1", LotNumber);
+                insertCommand.Parameters.AddWithValue("@Entry2", NameandDosage);
+                insertCommand.Parameters.AddWithValue("@Entry3", count);
+                insertCommand.Parameters.AddWithValue("@Entry4", expirationdate);
+                insertCommand.Parameters.AddWithValue("@Entry5", isExpired);
+                try
+                {
+                    insertCommand.ExecuteReader();
+                }
+                catch (SqliteException error)
+                {
+                    Console.WriteLine("Exception: " + error);
+                    check = false;
+                }
+                db.Close();
+            }
+            return check;
+        }
+        // Method to insert new Employees into the Employee table
+
+        public static bool Add_Employee(object sender, RoutedEventArgs e, string empID, string pin, string isActive)
+        {
+            bool check = true;
+            using (SqliteConnection db = new SqliteConnection("Filename=SamplesDB.db"))
+            {
+                db.Open();
+                SqliteCommand insertCommand = new SqliteCommand
+                {
+                    Connection = db,
+
+                    //Use parameterized query to prevent SQL injection attacks
+                    CommandText = "INSERT INTO Login VALUES (@Entry1, @Entry2, @Entry3);"
+                };
+                insertCommand.Parameters.AddWithValue("@Entry1", empID);
+                insertCommand.Parameters.AddWithValue("@Entry2", pin);
+                insertCommand.Parameters.AddWithValue("@Entry3", isActive);
+                try
+                {
+                    insertCommand.ExecuteReader();
+                }
+                catch (SqliteException error)
+                {
+                    Console.WriteLine(error);
+                    check = false;
+                }
+                db.Close();
+            }
+            return check;
+        }
+        // Method to insert log line into Log table
+
+        public static bool Add_Log(object sender, RoutedEventArgs e, string empID, string LotNumber, string patientID, string RepID, string LogType)
+        {
+            bool check = true;
+
+            return check;
+        }
+        // Method to insert text into the SQLite database
+        public static void Add_Text(object sender, RoutedEventArgs e, string inputVal)
+        {
+            using (SqliteConnection db = new SqliteConnection("Filename=SamplesDB.db"))
+            {
+                db.Open();
+                SqliteCommand insertCommand = new SqliteCommand
+                {
+                    Connection = db,
+
+                    //Use parameterized query to prevent SQL injection attacks
+                    CommandText = "INSERT INTO Sample VALUES (NULL, @Entry);"
                 };
                 insertCommand.Parameters.AddWithValue("@Entry", inputVal);
                 try
@@ -78,38 +248,30 @@ namespace InventorySystem.SQL
                 }
                 catch (SqliteException error)
                 {
-                    //Handle error
+                    Console.WriteLine(error);
                     return;
                 }
                 db.Close();
             }
         }
 
-        // Method to grab Text_Entry column from MyTable table in SQLite database
-        public static List<String> Grab_Entries_col()
+        // Method to import/export database
+        public static void ExportDB(string sourceFile, string destinationFile, string mode)
         {
-            List<string> entries = new List<string>();
-            using (SqliteConnection db = new SqliteConnection("Filename=sqliteSample.db"))
+            string LocalState = @"C:\Users\cyan\AppData\Local\Packages\704c98f6-3551-4a96-b6f6-f78cdab03ea8_q1j7n9hdrajb0\LocalState";
+            //var LocalState = Windows.Storage.ApplicationData.Current.LocalFolder;
+
+            string activeDB = LocalState + @"\SamplesDB.db";
+            string bakDB = activeDB + DateTime.Now.Ticks + ".bak";
+            if (mode == "import")
             {
-                db.Open();
-                SqliteCommand selectCommand = new SqliteCommand("SELECT Text_Entry from MyTable", db);
-                SqliteDataReader query;
-                try
-                {
-                    query = selectCommand.ExecuteReader();
-                }
-                catch (SqliteException error)
-                {
-                    //Handle error
-                    return entries;
-                }
-                while (query.Read())
-                {
-                    entries.Add(query.GetString(0));
-                }
-                db.Close();
+                System.IO.File.Copy(activeDB, bakDB, true);
+                System.IO.File.Copy(sourceFile, activeDB, true);
             }
-            return entries;
+            else if (mode == "export")
+            {
+                System.IO.File.Copy(sourceFile, destinationFile, true);
+            }
         }
     }
 }
