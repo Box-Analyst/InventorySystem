621314323983850b530ef6648cb719e712455170
AnalogCyan <git@thayn.me>
2020-11-23 15:57:13 -0600

    Added logs export button.

--- InventorySystem/Views/Settings/SettingsView.xaml.cs
+++ InventorySystem/Views/Settings/SettingsView.xaml.cs
@@ -288,5 +288,55 @@ namespace InventorySystem.Views.Settings
             }
             else PrivilegeError();
         }
+
+        private async void ExportLogs_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsAdmin())
+            {
+                var activeDB = await Windows.Storage.ApplicationData.Current.LocalFolder.GetFileAsync("SamplesDB.db");
+                var buffer = await Windows.Storage.FileIO.ReadBufferAsync(activeDB);
+
+                var savePicker = new Windows.Storage.Pickers.FileSavePicker
+                {
+                    SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.ComputerFolder
+                };
+                // Dropdown of file types the user can save the file as
+                savePicker.FileTypeChoices.Add("Plain Text", new List<string>() { ".txt" });
+                // Default file name if the user does not type one in or select a file to replace
+                savePicker.SuggestedFileName = "LOGS";
+
+                // Fetch list of all logs
+                List<string> logs = SQL.ManageDB.Grab_Entries("Log", "LogEntryID", "LogEntryID", null);
+
+                Windows.Storage.StorageFile file = await savePicker.PickSaveFileAsync();
+                if (file != null)
+                {
+                    // Prevent updates to the remote version of the file until
+                    // we finish making changes and call CompleteUpdatesAsync.
+                    Windows.Storage.CachedFileManager.DeferUpdates(file);
+                    // write to file
+                    await Windows.Storage.FileIO.WriteLinesAsync(file, logs);
+                    // Let Windows know that we're finished changing the file so
+                    // the other app can update the remote version of the file.
+                    // Completing updates may require Windows to ask for user input.
+                    Windows.Storage.Provider.FileUpdateStatus status =
+                        await Windows.Storage.CachedFileManager.CompleteUpdatesAsync(file);
+                    if (status == Windows.Storage.Provider.FileUpdateStatus.Complete)
+                    {
+                        ExportSuccess.Text = "File " + file.Name + " was saved.";
+                    }
+                    else
+                    {
+                        ExportSuccess.Text = "File " + file.Name + " couldn't be saved.";
+                    }
+                }
+                else
+                {
+                    ExportSuccess.Text = "Operation cancelled or interrupted.";
+                }
+                ExportSuccess.Visibility = Visibility.Visible;
+            }
+            else PrivilegeError();
+        }
     }
 }
